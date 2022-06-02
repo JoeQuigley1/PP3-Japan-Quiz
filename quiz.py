d95a30a81a94f584f2f1672e7c76fd6b742c5033
JoeQuigley1 <joeyjtq@hotmail.com>
2022-06-02 17:50:36 +0000

    Add question 2 to the game

--- quiz.py
+++ quiz.py
@@ -26,7 +26,7 @@ def start_quiz():
 
         answer1 = input("Please answer here: ")
 
-        if answer1.upper() ==  "B" or answer1 == "Tokyo":
+        if answer1.upper() ==  "B" or answer1.capitalize() == "Tokyo":
             print( "\nYou got the right answer! Well done!\n\n")
             score += 1
             break
@@ -34,10 +34,30 @@ def start_quiz():
             print("\nIncorrect answer!\n\n")
             break
         else:
-            print(f"Invalid data {answer1}. Please try again! \n" )
-    print("Answer: Tokyo \n \n")
-    print("Tokyo Metropolis is a large metropolitan area that is made up of 23 cities (called ku in Japanese) in central Tokyo")
-    print("and a futher 26 cities (referred to a shi in Japanese) outside of central Tokyo")
+            print(f"Invalid data: {answer1}. Please try again! \n" )
+    print(f"Answer: Tokyo \nYour score: {score}/10 \n")
+    print("Tokyo Metropolis is a large metropolitan area that is made up of 23 cities, (called ku in Japanese) in central Tokyo")
+    print("and a futher 26 cities, (referred to a shi in Japanese) outside of central Tokyo")
 
+    #Question 2
+    while True:
+        print("What is the population of Japan? \n")
+        print("A: 126 million, B: 94 million, C: 77 million ")
 
+        answer1 = input("Please answer here: ")
 
+        if answer1.upper() ==  "A" or answer1.upper() == "126 million":
+            print( "\nYou got the right answer! Well done!\n\n")
+            score += 1
+            break
+        elif answer1.upper() == "B" or answer1.upper() == "C":
+            print("\nIncorrect answer!\n\n")
+            break
+        #Need to find a way to get this to work
+        #elif answer1.upper() == "94 million" or answer1.upper() == "77 million":
+            print("\nIncorrect answer!\n\n")
+            break
+        else:
+            print(f"Invalid data: {answer1}. Please try again! \n" )
+    print(f"Answer: 126 million \nYour score: {score}/10 \n")
+    
